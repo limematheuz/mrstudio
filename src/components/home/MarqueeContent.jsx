3db8120dc02b383f3ea85558b09a9928b27307f0
limematheuz <mribeiro17.info@gmail.com>
2026-03-15 22:03:32 +0100

    feat: add Pedro's Remodeling NC logo to marquee

--- src/components/home/MarqueeContent.jsx
+++ src/components/home/MarqueeContent.jsx
@@ -1,5 +1,6 @@
 import KernelAuditLogo from '../../assets/kernel-audit.svg';
 import RemodelingLogo from '../../assets/remodeling-logo.svg';
+import PedrosLogo from '../../assets/pedros-remodeling-logo.svg';
 import InnovaticLogo from '../../assets/innovatic-logo.svg';
 import ElhaLogo from '../../assets/elha-logo.svg';
 
@@ -10,6 +11,7 @@ export default function MarqueeContent() {
     return (
         <div style={{ display: 'flex', width: '100%', justifyContent: 'space-around', alignItems: 'center' }}>
             <span style={wrapStyle}><img src={RemodelingLogo} alt="RRJ Remodeling" style={{ ...logoStyle, height: '3rem' }} /></span>
+            <span style={wrapStyle}><img src={PedrosLogo} alt="Pedro's Remodeling NC" style={{ ...logoStyle, height: '2.8rem' }} /></span>
             <span style={wrapStyle}><img src={InnovaticLogo} alt="Innovatic" style={{ ...logoStyle, height: '2rem' }} /></span>
             <span style={wrapStyle}><img src={ElhaLogo} alt="Elha Laser Center" style={{ ...logoStyle, height: '1.6rem' }} /></span>
             <span style={wrapStyle}><img src={KernelAuditLogo} alt="Kernel Audit Auditores" style={{ ...logoStyle, height: '2.2rem' }} /></span>
